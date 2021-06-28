913685232519bec1d9ead1c96ced7e5fed87da6e
Salvatore Testa <sal.testa@gmail.com>
2021-06-27 18:35:11 -0700

    Scrape the IHOP website for the list of locations
    Check-in a copy of the list after sorting it alphabetically.

--- ihop_scraper.py
+++ ihop_scraper.py
@@ -6,10 +6,28 @@ class IHOPScraper(scrapy.Spider):
     start_urls = ['https://restaurants.ihop.com/']
 
     def parse(self, response, **kwargs):
-        for subdivision in response.css('div.map-list-item').css('a.ga-link'):
-            subdivision_page = subdivision.attrib['href']
-            subdivision_name = subdivision.root.text
-            yield {
-                'subdivision': subdivision_name,
-                'page': subdivision_page
-            }
+        yield from response.follow_all(css='div.map-list-item a.ga-link', callback=self.parse_subdivision)
+
+    def parse_subdivision(self, response):
+        yield from response.follow_all(css='div.map-list-item a.ga-link', callback=self.parse_city)
+
+    def parse_city(self, response):
+        yield from response.follow_all(css='div.map-list-item a.ga-link', callback=self.parse_store)
+
+    @staticmethod
+    def parse_store(response):
+        hours = list(map(lambda row: {
+            'day': row.xpath(".//span/@data-daypart").get(),
+            'open': row.css("span.time-open::text").get(),
+            'close': row.css("span.time-close::text").get()
+        }, list(response.css('div.hide-mobile div.day-hour-row'))))
+        subdivision_abbr, subdivision = response.xpath('//meta[@name="state"]/@content').get().split(", ")
+        yield {
+            'subdivision': subdivision,
+            'subdivision_abbr': subdivision_abbr,
+            'city': response.xpath('//meta[@name="city"]/@content').get(),
+            'zip': response.xpath('//meta[@name="zip"]/@content').get(),
+            'address': response.xpath('//meta[@name="address"]/@content').get(),
+            'url': response.url,
+            'hours': hours
+        }
